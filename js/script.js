6534147da5256427963cdbea7b675722d8231f12
madiinemm <uraimovamadina179@gmail.com>
2023-11-27 17:41:02 +0500

    masalalar ishlandi

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// 1. let a = +prompt ("4^2");
+// let b = +prompt ("8^2");
+
+// if (a>b) { 
+//     console.log(a);
+// } else {
+//     console.log(b);
+// }
+
+// 2. let a = +prompt ("12");
+// let b = +prompt ("20");
+// let c = +prompt ("15");
+// if (a,b>b) {
+//     console.log(a,b)
+// }
+// else {
+//      console.log(b);
+// }
+
+// 3. let a = +prompt ("12");
+
+// if (a%2==0) {
+//     console.log("juft son");
+// }
+// else {
+//     console.log("toq son");
+// }
+
+
+
